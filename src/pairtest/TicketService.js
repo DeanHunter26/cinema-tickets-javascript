972fefbcd255cd110f1a77d23ebbbd0822b997cc
Dean Hunter <deanhunter30@gmail.com>
2023-07-28 21:30:11 +0100

    format code and added validate max tickets

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,27 +1,32 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 const ERROR_MESSAGES = {
-  INVALID_ACCOUNT_ID: 'Invalid account ID. Account ID must be a positive integer.',
-  NO_TICKETS_PROVIDED: 'No tickets, provided. Please provide at least one ticket type request.',
-  INVALID_TICKET_TYPE_REQUESTS: 'Invalid ticket type requests. All items in the ticketTypeRequsts array must be instances of TicketTypeRequest.',
-  TICKET_VALIDATION_FAILED: 'Ticket validation failed. One or more tickets are invalid.',
+  INVALID_ACCOUNT_ID:
+    "Invalid account ID. Account ID must be a positive integer.",
+  NO_TICKETS_PROVIDED:
+    "No tickets, provided. Please provide at least one ticket type request.",
+  INVALID_TICKET_TYPE_REQUESTS:
+    "Invalid ticket type requests. All items in the ticketTypeRequsts array must be instances of TicketTypeRequest.",
+  TICKET_VALIDATION_FAILED:
+    "Ticket validation failed. One or more tickets are invalid.",
+  MAX_TICKETS_EXCEEDED: "Number of tickets cannot exceed 20.",
 };
 
 export default class TicketService {
   #validateAccountId(accountId) {
-    if(!Number.isInteger(accountId) || accountId < 1) {
+    if (!Number.isInteger(accountId) || accountId < 1) {
       throw new TypeError(ERROR_MESSAGES.INVALID_ACCOUNT_ID);
     }
-  } 
+  }
 
   #validateTicketTypeRequests(ticketTypeRequests) {
-    if(ticketTypeRequests.length === 0) {
+    if (ticketTypeRequests.length === 0) {
       throw new RangeError(ERROR_MESSAGES.NO_TICKETS_PROVIDED);
     }
 
-    for(const request of ticketTypeRequests) {
-      if(!(request instanceof TicketTypeRequest)) {
+    for (const request of ticketTypeRequests) {
+      if (!(request instanceof TicketTypeRequest)) {
         throw new TypeError(ERROR_MESSAGES.INVALID_TICKET_TYPE_REQUESTS);
       }
     }
@@ -30,14 +35,31 @@ export default class TicketService {
   #groupAndCountTickets(ticketTypeRequests) {
     const ticketTypeCounts = new Map();
 
-    for(const request of ticketTypeRequests) {
+    for (const request of ticketTypeRequests) {
       const type = request.getTicketType();
       const noOfTickets = request.getNoOfTickets();
 
-      ticketTypeCounts.set(type, (ticketTypeCounts.get(type) || 0) + noOfTickets);
+      ticketTypeCounts.set(
+        type,
+        (ticketTypeCounts.get(type) || 0) + noOfTickets
+      );
     }
 
     return ticketTypeCounts;
+  }
+
+  #validateMaxTickets(ticketTypeCounts) {
+    let totalTickets = 0;
+
+    for (const count of ticketTypeCounts.values()) {
+      totalTickets += count;
+    }
+
+    const maxTickets = 20;
+
+    if (totalTickets > maxTickets) {
+      throw new InvalidPurchaseException(ERROR_MESSAGES.MAX_TICKETS_EXCEEDED);
+    }
   }
 
   /**
@@ -50,6 +72,6 @@ export default class TicketService {
     this.#validateTicketTypeRequests(ticketTypeRequests);
 
     const ticketTypeCounts = this.#groupAndCountTickets(ticketTypeRequests);
-   // this.#validateTickets();
+    this.#validateMaxTickets(ticketTypeCounts);
   }
 }
--- test/TicketService.spec.js
+++ test/TicketService.spec.js
@@ -1,47 +1,84 @@
 import TicketService from "../src/pairtest/TicketService";
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
 
-describe('TicketService', () => {
-    let ticketService;
+describe("TicketService", () => {
+  let ticketService;
 
-    beforeEach(() => {
-        ticketService = new TicketService();
+  beforeEach(() => {
+    ticketService = new TicketService();
+  });
+
+  describe("purchaseTickets", () => {
+    const typeRequest = new TicketTypeRequest("ADULT", 1);
+
+    describe("validateAccountId", () => {
+      const INVALID_ACCOUNT_ID_ERROR =
+        "Invalid account ID. Account ID must be a positive integer.";
+
+      it("should throw a type error when accountId is undefined", () => {
+        expect(() =>
+          ticketService.purchaseTickets(undefined, typeRequest)
+        ).toThrow(INVALID_ACCOUNT_ID_ERROR);
+      });
+
+      it("should throw a type error when accountId is a string", () => {
+        expect(() => ticketService.purchaseTickets("cle", typeRequest)).toThrow(
+          INVALID_ACCOUNT_ID_ERROR
+        );
+      });
+
+      it("should throw a type error when accountId is not an integer", () => {
+        expect(() => ticketService.purchaseTickets(4.23, typeRequest)).toThrow(
+          INVALID_ACCOUNT_ID_ERROR
+        );
+      });
+
+      it("should throw a type error when accountId is less than one", () => {
+        expect(() => ticketService.purchaseTickets(0, typeRequest)).toThrow(
+          INVALID_ACCOUNT_ID_ERROR
+        );
+      });
     });
 
-    describe('purchaseTickets', () => {
-        const typeRequest = new TicketTypeRequest('ADULT', 1);
+    describe("validateTicketTypeRequests", () => {
+      const NO_TICKETS_PROVIDED_ERROR =
+        "No tickets, provided. Please provide at least one ticket type request.";
+      const INVALID_TICKET_TYPE_REQUESTS_ERROR =
+        "Invalid ticket type requests. All items in the ticketTypeRequsts array must be instances of TicketTypeRequest.";
 
-        describe('validateAccountId', () => {
-            const INVALID_ACCOUNT_ID_ERROR = 'Invalid account ID. Account ID must be a positive integer.';
+      it("should throw a range error when no tickets provided", () => {
+        expect(() => ticketService.purchaseTickets(123)).toThrow(
+          NO_TICKETS_PROVIDED_ERROR
+        );
+      });
 
-            it('should throw a type error when accountId is undefined', () => {
-              expect(() => ticketService.purchaseTickets(undefined, typeRequest)).toThrow(INVALID_ACCOUNT_ID_ERROR);
-            });
-        
-            it('should throw a type error when accountId is a string', () => {
-              expect(() => ticketService.purchaseTickets('cle', typeRequest)).toThrow(INVALID_ACCOUNT_ID_ERROR);
-            });
-        
-            it('should throw a type error when accountId is not an integer', () => {
-              expect(() => ticketService.purchaseTickets(4.23, typeRequest)).toThrow(INVALID_ACCOUNT_ID_ERROR);
-            });
-        
-            it('should throw a type error when accountId is less than one', () => {
-              expect(() => ticketService.purchaseTickets(0, typeRequest)).toThrow(INVALID_ACCOUNT_ID_ERROR);
-            });
-        });
-
-        describe('validateTicketTypeRequests', () => {
-          const NO_TICKETS_PROVIDED_ERROR = 'No tickets, provided. Please provide at least one ticket type request.';
-          const INVALID_TICKET_TYPE_REQUESTS_ERROR = 'Invalid ticket type requests. All items in the ticketTypeRequsts array must be instances of TicketTypeRequest.';
-    
-          it('should throw a range error when no tickets provided', () => {
-            expect(() => ticketService.purchaseTickets(123)).toThrow(NO_TICKETS_PROVIDED_ERROR);
-          });
-    
-          it('should throw a type error when not an array of TicketTypeRequest objects', () => {
-            expect(() => ticketService.purchaseTickets(123, [], 'ssks', 123, new TicketTypeRequest('ADULT', 1))).toThrow(INVALID_TICKET_TYPE_REQUESTS_ERROR);
-          });
-        });
+      it("should throw a type error when not an array of TicketTypeRequest objects", () => {
+        expect(() =>
+          ticketService.purchaseTickets(
+            123,
+            [],
+            "ssks",
+            123,
+            new TicketTypeRequest("ADULT", 1)
+          )
+        ).toThrow(INVALID_TICKET_TYPE_REQUESTS_ERROR);
+      });
     });
+
+    describe("validateMaxTickets", () => {
+      const MAX_TICKETS_EXCEEDED = "Number of tickets cannot exceed 20.";
+
+      it("should throw an error when the total tickets are more than 20", () => {
+        const ticketTypeRequests = [
+          new TicketTypeRequest("ADULT", 12),
+          new TicketTypeRequest("CHILD", 10),
+          new TicketTypeRequest("INFANT", 1),
+        ];
+
+        expect(() =>
+          ticketService.purchaseTickets(123, ...ticketTypeRequests)
+        ).toThrow(MAX_TICKETS_EXCEEDED);
+      });
+    });
+  });
 });
